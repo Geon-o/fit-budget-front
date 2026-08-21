1e43e0ced209fb77150c12aa33388799c92b4d1f
진건오 <shjwin96@bizbee.co.kr>
2026-08-21 17:28:38 +0900

    style: 상단 네비게이션 계정 영역·메뉴 배치 정리

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -32,28 +32,29 @@ function Layout() {
             <Logo />
           </NavLink>
           {!isLedgerListPage && (
-            <>
+            <nav className="app-nav__center">
+              {NAV_ITEMS.map((item) => (
+                <NavLink
+                  key={item.to}
+                  to={item.to}
+                  end={item.end}
+                  className={({ isActive }) =>
+                    isActive ? 'app-nav__link app-nav__link--active' : 'app-nav__link'
+                  }
+                >
+                  {item.label}
+                </NavLink>
+              ))}
+              <span className="app-nav__center-divider" />
               <NavLink to="/ledgers" className="app-nav__ledger-button">
                 가계부 변경
               </NavLink>
-              <nav className="app-nav__links">
-                {NAV_ITEMS.map((item) => (
-                  <NavLink
-                    key={item.to}
-                    to={item.to}
-                    end={item.end}
-                    className={({ isActive }) =>
-                      isActive ? 'app-nav__link app-nav__link--active' : 'app-nav__link'
-                    }
-                  >
-                    {item.label}
-                  </NavLink>
-                ))}
-              </nav>
-            </>
+            </nav>
           )}
           <div className="app-nav__account">
-            {user && <span className="app-nav__email">{user.nickname}</span>}
+            <span className="app-nav__avatar">{user.nickname.charAt(0)}</span>
+            <span className="app-nav__nickname">{user.nickname}</span>
+            <span className="app-nav__divider" />
             <button type="button" className="app-nav__logout" onClick={handleLogout}>
               로그아웃
             </button>
